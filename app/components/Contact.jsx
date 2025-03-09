f2d57d3c102fb132fcb01a321808ddba998b0026
atharianr <atharianrahmadani@gmail.com>
2025-03-10 00:18:54 +0700

    change key name

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         setResult("Sending....");
         const formData = new FormData(event.target);
 
-        formData.append("access_key", process.env.web3FormsAccessKey);
+        formData.append("access_key", process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY);
 
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
