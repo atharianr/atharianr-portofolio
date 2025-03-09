4a8c6ef51c35eb0db8dea4758851ed70106f8d9b
atharianr <atharianrahmadani@gmail.com>
2025-03-10 00:29:24 +0700

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://atharianr.dev', // Ganti sama domain kamu
+    generateRobotsTxt: true,          // Generate robots.txt otomatis
+    sitemapSize: 7000,                // Default aman
+    changefreq: 'monthly',            // Sesuai kebutuhan
+    priority: 0.7,                    // Prioritas crawling
+  };
+  
