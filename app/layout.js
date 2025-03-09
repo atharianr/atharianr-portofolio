0968fbdfb91b987916269f385240eaf42635d08a
atharianr <atharianrahmadani@gmail.com>
2025-03-09 23:56:18 +0700

    add icon for web

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,11 @@ const poppins = Poppins({
 export const metadata = {
   title: "atharianr.",
   description: "A portofolio from Atharian Rahmadani, Software Developer based in Surabaya, Indonesia",
+  icons: {
+    icon: "/atharianr_icon.svg", // path to your favicon inside the /public folder
+    shortcut: "/atharianr_icon.svg", // optional: often the same as icon
+    apple: "/atharianr_icon.svg" // optional: for Apple devices (if you have it)
+  }
 };
 
 export default function RootLayout({ children }) {
